8c75ea239a9e883a0f987a8e69445f814255b8ad
berserker09 <93809440+berserker09@users.noreply.github.com>
2021-11-06 15:44:22 -0600

    Update Sbot.js

--- Sbot.js
+++ Sbot.js
@@ -1,26 +1,46 @@
-const { WAConnection } = require('@adiwajshing/baileys');
-const fs = require('fs');
-
-async function iniciar () { 
-        const client = new WAConnection()
-
-        client.logger.level = 'warn'
-
-        client.on('qr', () => {
-        })
-
-        fs.existsSync('./Samu330.json') && client.loadAuthInfo('./Samu330.json')
-
-        client.on('connecting', () => {
-        console.log('Conectando')
-        })
-
-        client.on('open', () => {
-        console.log('Conectado exitosamente :D')
-        })
-        await client.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./Samu330.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
-        }
-
-iniciar ()
-.catch (err => console.log("A ocurrido un error: " + err))
+const { 
+    WAConnection,
+    MessageType,
+    Presence, 
+    MessageOptions,
+    Mimetype,
+    WALocationMessage,
+    WA_MESSAGE_STUB_TYPES,
+    ReconnectMode,
+    ProxyAgent,
+    GroupSettingChange,
+    ChatModification,
+    waChatKey,
+    WA_DEFAULT_EPHEMERAL,
+    mentionedJid,
+    prepareMessageFromContent, 
+    Browsers,
+    processTime
+    } = require("@adiwajshing/baileys")
+  const crypto = require("crypto-js");        
+  const CryptoJS = require("crypto-js");
+  const fs = require('fs');
+  const prefix = '.'
+  const moment = require("moment-timezone");
+  const yts = require('yt-search')
+  const LeoGg = require('google-it');
+  const LeoGgImg = require('g-i-s');
+  const fetch = require('node-fetch');
+  const _registered = JSON.parse(fs.readFileSync('./src/registered.json'))
+  const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
+  const antilink = JSON.parse(fs.readFileSync('./src/antilink.json'))
+  const ban = JSON.parse(fs.readFileSync('./src/banned.json'))
+  const conn = require("./lib/connect")
+  const wa = require("./lib/wa")
+  const { color } = require("./lib/color");
+  const {  getBuffer, h2k,  generateMessageID, getGroupAdmins,  getRandom, banner,  start,  info, success, close,} = require("./lib/functions");
+  const { getLevelingXp, getLevelingId, addLevelingXp, addLevelingLevel, addLevelingId, getLevelingLevel, getUserRank, addCooldown, leveltab } = require('./lib/leveling.js')
+  const { addBanned, unBanned, BannedExpired, cekBannedUser } = require('./lib/banned.js')
+  //const {convertSticker} = require("./lib/swm.js")
+  const {y2mateA, y2mateV} = require('./lib/y2mate.js')
+  const { isFiltered, addFilter } = require('./lib/Addfilter')
+  const config = JSON.parse(fs.readFileSync("./config.json"))
+  const owner = config.owner
+  const mods = config.mods
+  const fake = 'client'
+  var public = config.public
