314479ba9106444f15464d4d751f70df5ece630d
berserker09 <93809440+berserker09@users.noreply.github.com>
2021-11-06 00:32:49 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+clear
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+
+echo "todos los paquetes se an instalado correctamente "
+
+echo "Hecho, usa npm start para iniciar senpai >w< "
+
