9a0d34ed350467517a906a990a715a541dcc65c1
berserker09 <93809440+berserker09@users.noreply.github.com>
2021-11-06 23:29:51 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,16 +12,8 @@ apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-npm i
-npm i
 
 
-echo -e '\033[0m\033[1;35m
-  ____                                             __ "        
- /\  _`\                                          /\ \ "       
- \ \ \L\ \     __    _ __    ____     __    _ __  \ \ \/' "   
-  \ \  _ <'  /'__`\ /\`'__\ /',__\  /'__`\ /\`'__\ \ \ , < "   
-   \ \ \L\ \/\  __/ \ \ \/ /\__, `\/\  __/ \ \ \/   \ \ \\`\  "
-    \ \____/\ \____\ \ \_\ \/\____/\ \____\ \ \_\    \ \_\ \_\ "
-     \/___/  \/____/  \/_/  \/___/  \/____/  \/_/     \/_/\/_/ "
+echo "todos los paquetes se han instalado correctamente ✓ "
 
+echo "usa el comando npm start para iniciar el bot >w< "
