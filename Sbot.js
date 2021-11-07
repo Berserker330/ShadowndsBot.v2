d84f9f2ca074a6f420298d75036b3e5ab3ef872d
berserker09 <93809440+berserker09@users.noreply.github.com>
2021-11-07 11:51:36 -0600

    Update Sbot.js

--- Sbot.js
+++ Sbot.js
@@ -1,23 +1,246 @@
-const { WAConnection } = require("@adiwajshing/baileys")
-const chalk = require('chalk')
-const fs = require("fs")
-const exec = require('child_process')
+const {
+    WAConnection,
+    MessageType,
+    Presence,
+    Mimetype,
+    rugaapi,
+    GroupSettingChange
+} = require('@adiwajshing/baileys')
 
-const leo = new WAConnection()
-exports.leo = leo
+/******COMIENZO DE LA ENTRADA DEL ARCHIVO******/
+const { color, bgcolor } = require('./lib/color')
+const { bahasa } = require('./src/bahasa')
+const { negara } = require('./src/kodenegara')
+const { virtex } = require('./src/virtex')
+const { wait, pegatinas, musica, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
+const { fetchJson } = require('./lib/fetcher')
+const { recognize } = require('./lib/ocr')
+/******FIN DE ENTRADA DE ARCHIVO******/
 
-exports.connect = async() => {
-    leo.version = [2, 2143, 3]
-    console.log(chalk.keyword("blue")('⌛ 𝐂𝐎𝐍𝐄𝐂𝐓𝐀𝐍𝐃𝐎 ...'))
-    let auth = './Chui.json'
-    leo.logger.level = 'warn'
-    leo.on("qr", () => {
-       console.log(chalk.keyword("red")('𝐄𝐒𝐂𝐀𝐍𝐄𝐄 𝐄𝐋 𝐂𝐎𝐃𝐈𝐆𝐎 𝐐𝐑 𝐏𝐀𝐑𝐀 𝐂𝐎𝐍𝐄𝐂𝐓𝐀𝐑𝐒𝐄'))
-    })
-    fs.existsSync(auth) && leo.loadAuthInfo(auth)
-    leo.on('connecting', () => {
-        console.log(chalk.whiteBright("⌛"), chalk.keyword("red")(" "), chalk.keyword("aqua")("𝐄𝐒𝐂𝐀𝐍𝐄𝐄 𝐄𝐋 𝐂𝐎𝐃𝐈𝐆𝐎 𝐐𝐑 "))
-    })
-    await leo.connect({ timeoutMs: 30 * 1000 })
-    return leo
+/******COMIENZO DE LA ENTRADA DEL PAQUETE NPM******/
+const fs = require('fs')
+const moment = require('moment-timezone')
+const { exec } = require('child_process')
+const kagApi = require('@kagchi/kag-api')
+const axios = require("axios")
+const fetch = require('node-fetch')
+/*const tiktod = require('tiktok-scraper')*/
+const ffmpeg = require('fluent-ffmpeg')
+const { removeBackgroundFromImageFile } = require('remove.bg')
+/*const imgbb = require('imgbb-uploader')*/
+const lolis = require('lolis.life')
+const loli = new lolis()
+const speed = require('performance-now')
+/******FIN DE ENTRADA DEL PAQUETE NPM******/
+
+/******COMIENZO DE LA ENTRADA JSON******/
+const welkom = JSON.parse(fs.readFileSync('./database/json/welkom.json'))
+const nsfw = JSON.parse(fs.readFileSync('./database/json/nsfw.json'))
+const ban = JSON.parse(fs.readFileSync('./database/banned.json'))
+const setting = JSON.parse(fs.readFileSync('./src/settings.json'))
+const samih = JSON.parse(fs.readFileSync('./database/json/simi.json'))
+const user = JSON.parse(fs.readFileSync('./database/json/user.json'))
+const _leveling = JSON.parse(fs.readFileSync('./database/json/leveling.json'))
+const _level = JSON.parse(fs.readFileSync('./database/json/level.json'))
+/******FIN DE ENTRADA JSON******/
+
+/******INICIO DE LA ENTRADA DEL MENÚ******/
+const { help } = require('./src/help')
+const { logomaker } = require('./database/menu/logomaker')
+const { toinmenu } = require('./src/toinmenu')
+const { menuadmin } = require('./src/menuadmin')
+const { nsfwmenu } = require('./src/nsfwmenu')
+const { desmenu } = require('./src/desmenu')
+const { version } = require('./src/version')
+const { juegos } = require('./src/juegos')
+const { shantera } = require('./src/shantera')
+const { antimenu } = require('./src/antimenu')
+const { welmenu } = require('./src/welmenu')
+const { banmenu } = require('./src/banmenu')
+const { otak } = require('./src/otak')
+const { levelmenu } = require('./src/levelmenu')
+/******FIN DE ENTRADA DEL MENÚ******/
+
+/******CARGA DE ENTRADA VCARD******/
+const vcard = 'BEGIN:VCARD\n' // Tarjeta de contacto
+            + 'VERSION:3.0\n' 
+            + 'FN:Shan\n' // Nombre
+            + 'ORG:Shanduy;\n' // Propietario
+            + 'TEL;type=CELL;type=VOICE;waid=593967689722:+593 96 768 9722\n' // ID de WhatsApp + número de teléfono
+            + 'END:VCARD'
+/******FIN DE ENTRADA VCARD******/
+
+prefix = '/'
+blocked = []
+banChats = false
+
+/******CONFIGURACION DE CARGA******/
+const settingan = JSON.parse(fs.readFileSync('./admin/set.json'))
+const {
+	author,
+	pack
+} = settingan
+
+/******INICIO DE FUNCIONES ENTRADA******/
+
+/******ARCHIVOS ANTILINK POR SHANDUY******/
+
+const antilink = JSON.parse(fs.readFileSync('./src/antilink.json'))
+const antiface = JSON.parse(fs.readFileSync('./src/antiface.json'))
+const antitik = JSON.parse(fs.readFileSync('./src/antitik.json'))
+const antinsta = JSON.parse(fs.readFileSync('./src/antinsta.json'))
+const antikwai = JSON.parse(fs.readFileSync('./src/antikwai.json'))
+const antiwa = JSON.parse(fs.readFileSync('./src/antiwa.json'))
+const antidiscord = JSON.parse(fs.readFileSync('./src/antidiscord.json'))
+
+/******FIN DE ARCHIVOS ANTILINK POR SHANDUY******/
+
+
+//LEVEL INICIO
+const getLevelingXp = (userId) => {
+            let position = false
+            Object.keys(_level).forEach((i) => {
+                if (_level[i].jid === userId) {
+                    position = i
+                }
+            })
+            if (position !== false) {
+                return _level[position].xp
+            }
+        }
+
+        const getLevelingLevel = (userId) => {
+            let position = false
+            Object.keys(_level).forEach((i) => {
+                if (_level[i].jid === userId) {
+                    position = i
+                }
+            })
+            if (position !== false) {
+                return _level[position].level
+            }
+        }
+
+        const getLevelingId = (userId) => {
+            let position = false
+            Object.keys(_level).forEach((i) => {
+                if (_level[i].jid === userId) {
+                    position = i
+                }
+            })
+            if (position !== false) {
+                return _level[position].jid
+            }
+        }
+
+        const addLevelingXp = (userId, amount) => {
+            let position = false
+            Object.keys(_level).forEach((i) => {
+                if (_level[i].jid === userId) {
+                    position = i
+                }
+            })
+            if (position !== false) {
+                _level[position].xp += amount
+                fs.writeFileSync('./database/json/level.json', JSON.stringify(_level))
+            }
+        }
+
+        const addLevelingLevel = (userId, amount) => {
+            let position = false
+            Object.keys(_level).forEach((i) => {
+                if (_level[i].jid === userId) {
+                    position = i
+                }
+            })
+            if (position !== false) {
+                _level[position].level += amount
+                fs.writeFileSync('./database/json/level.json', JSON.stringify(_level))
+            }
+        }
+
+        const addLevelingId = (userId) => {
+            const obj = {jid: userId, xp: 1, level: 1}
+            _level.push(obj)
+            fs.writeFileSync('./database/json/level.json', JSON.stringify(_level))
+        }
+//LEVEL FIN
+	
+function addMetadata(packname, author) {	
+	if (!packname) packname = 'ShanBot'; if (!author) author = 'shanduy';	
+	author = author.replace(/[^a-zA-Z0-9]/g, '');	
+	let name = `${author}_${packname}`
+	if (fs.existsSync(`./${name}.exif`)) return `./${name}.exif`
+	const json = {	
+		"sticker-pack-name": packname,
+		"sticker-pack-publisher": author,
+	}
+	const littleEndian = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00])	
+	const bytes = [0x00, 0x00, 0x16, 0x00, 0x00, 0x00]	
+
+	let len = JSON.stringify(json).length	
+	let last	
+
+	if (len > 256) {	
+		len = len - 256	
+		bytes.unshift(0x01)	
+	} else {	
+		bytes.unshift(0x00)	
+	}	
+
+	if (len < 16) {	
+		last = len.toString(16)	
+		last = "0" + len	
+	} else {	
+		last = len.toString(16)	
+	}	
+
+	const buf2 = Buffer.from(last, "hex")	
+	const buf3 = Buffer.from(bytes)	
+	const buf4 = Buffer.from(JSON.stringify(json))	
+
+	const buffer = Buffer.concat([littleEndian, buf2, buf3, buf4])	
+
+	fs.writeFile(`./${name}.exif`, buffer, (err) => {	
+		return `./${name}.exif`	
+	})	
+
+} 
+	
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `${pad(hours)} Horas ${pad(minutes)} Minutos ${pad(seconds)} Segundos`
 }
+
+async function starts() {
+	const client = new WAConnection()
+	client.version = [3, 3234, 9]
+        client.logger.level = 'warn'
+	console.log(banner.string)
+	client.on('qr', () => {
+		console.log(color('[','white'), color('!','red'), color(']','white'), color('Escanea el codigo QR rapido!!!'))
+	})
+
+	fs.existsSync('./Nazwa.json') && client.loadAuthInfo('./Nazwa.json')
+	client.on('connecting', () => {
+		start('2', 'Estas desconectado')
+	})
+	client.on('open', () => {
+		success('2', 'Conectado by shanduy')
+	})
+	await client.connect({timeoutMs: 30*1000})
+        fs.writeFileSync('./Nazwa.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+
+	client.on('group-participants-update', async (anu) => {
+		if (!welkom.includes(anu.jid)) return
+		try {
+			const mdata = await client.groupMetadata(anu.jid)
+			console.log(anu)
+			if (anu.action == 'add') {
